9f3af52f4e23453792af2643d263c09abe70f949
Hyde <jojo1206@protonmail.com>
2022-04-28 19:10:01 +0900

    지움

--- Frame128/Player.h
+++ Frame128/Player.h
@@ -46,10 +46,5 @@ private:
 	virtual void OnTriggerExit(CCollider * _pOther);
 private:
 	void CalcMat();
-private:
-	DXV3 m_vWheel[4];//바퀴의 좌표값을 저장해놓은 벡터
-	DXV3 m_vCoord[4];//바퀴의 로컬 * 플레이어 월드의 계산값을 받기 위한 좌표
-	float m_fWheelDist;
-	DXV3 m_vDirPos;
 };
 
